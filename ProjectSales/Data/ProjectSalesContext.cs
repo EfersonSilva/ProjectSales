cc64f23e92f2d2fde4537d397caf0a858afb0991
Eferson Silva <efersonds@gmail.com>
2021-01-08 13:06:15 -0300

    Add project files.

---
+++ ProjectSales/Data/ProjectSalesContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectSales.Models;
+
+namespace ProjectSales.Data
+{
+    public class ProjectSalesContext : DbContext
+    {
+        public ProjectSalesContext (DbContextOptions<ProjectSalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
+    }
+}
